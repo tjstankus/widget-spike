44baaa099367c50419cbaa9039298d590b7119ae
TJ Stankus <tj@stank.us>
2013-04-12 14:34:52 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PortalWidgetSpike::Application.config.secret_key_base = 'a3c694e9b602daef351906a8f9259c037528bb2da2502e99e1ca7b7e579709d90a0e5209b22049e411aaefe85e3cbd3e6ea57d75b2bfac64a6a318fff96fda30'
